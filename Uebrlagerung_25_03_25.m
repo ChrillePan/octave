706ad730a40c9e61150ccb5e428a168d911ba589
ChrillePan <jugendleiter@gmail.com>
2025-03-26 20:32:51 +0100

    Add files via upload
    CHG - Add kr and ke, simplification

--- Uebrlagerung_25_03_25.m
+++ Uebrlagerung_25_03_25.m
@@ -1,7 +1,9 @@
 % Konstanten
 V_max = 1e5;
-R1 = 1e3;
-R2 = 10e3;
+Z1 = 1e3;
+Z2 = 1e4;
+ke = - Z2 ./ (Z1+Z2); % Eingangs-Kenngröße
+kr = - Z1 ./ (Z1+Z2); % Ausgangs-Kenngröße
 omega_g = 10;
 omega = logspace (0, 10, 100);
 
@@ -13,21 +15,24 @@ omega = logspace (0, 10, 100);
   % in dB
   dB_V0 = 20 .* log10(V0); % Umwandlung in dB
 
-  % Uebertragungsfunktoon
-  Vu = -(R2./R1).* (1./ (1-(1./(V0*(R1./(R1+R2))))));
+  % Übertragungsfunktoon
+  %Vu = -(ke ./ kr)*(1 ./ (1-(1 ./ V0 * kr))); % Detaillierter aufgeschrieben
+  Vu = (V0*ke) ./ (1-(V0*kr)); % Einfachste Form
 
   % in dB
   dB_Vu = 20 * log10(Vu); % Umwandlung in dB
 
 % Plots
-
 semilogx(omega, dB_Vu, 'r', 'LineWidth', 1); % Rote Linie, dickere Darstellung
 hold on;
 semilogx(omega, dB_V0, 'g', 'LineWidth', 1); % Grüne Linie, dickere Darstellung
 grid on;
 
 % Diagramm- und Achsenbezeichnung
+title('Übung vom 25.03.2025', 'FontSize', 16);
+legend('Vu','V0', 'location', 'SouthWest', 'FontSize', 14);
 xlabel('Kreisfrequenz \omega (s^{-1})', 'FontSize', 14);
 ylabel('Verstärkung (dB)', 'FontSize', 14);
-title('Übung vom 25.03.2025', 'FontSize', 16);
-legend('Vu','V0');
+ylim([-150,150]);
+
+
